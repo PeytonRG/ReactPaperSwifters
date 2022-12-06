9341d600df2742f1a826b877455193a9bf736eed
Peyton Gasink <peyton@peytongasink.dev>
2022-12-06 12:26:32 -0600

    stubbed swiftui app

---
+++ rps-swiftui/ReactPaperSwifters/ReactPaperSwiftersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactPaperSwiftersApp.swift
+//  ReactPaperSwifters
+//
+//  Created by Peyton Gasink on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ReactPaperSwiftersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
